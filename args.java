635a10d8ff488ba1e266eaa2c238142fbf2f0693
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-14 16:22:30 +0600

    Add files via upload

---
+++ args.java
@@ -0,0 +1,22 @@
+package HelloApp;
+
+public class args{
+    
+    public static void main(String[] args) {
+        
+        int [] arr;
+
+        arr = new int[5];
+
+        arr[0] =100;
+        arr[1]=20;
+
+        for(int i= 0; i<arr.length; i++)
+        System.out.println("It is " +"index"+ i+ " = "+ arr[i]);
+        
+        
+
+
+
+    }
+}
