f14df74dab541d3e5d7dbd9a4f830f5cdac5a468
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-14 20:44:23 +0600

    Add files via upload

---
+++ mD.java
@@ -0,0 +1,14 @@
+package HelloApp;
+
+import java.io.*;
+
+class mD {
+    
+    public static void main(String[]args){
+        int[][] arr = new int [3][3];
+        System.out.println("row  "+ arr.length +  " column  "+ arr[0].length);;
+    }
+
+
+    
+}
