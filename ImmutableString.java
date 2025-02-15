eefab2e7d63d5f784654b09d7a2a9d1095c90150
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-15 13:59:54 +0600

    Add files via upload

---
+++ ImmutableString.java
@@ -0,0 +1,15 @@
+package String;
+
+public class ImmutableString {
+
+    public static void main(String[] args) {
+        
+        
+        String name= " Rifat ";
+        name.concat("Chowdhury");
+
+        System.out.println(name);
+
+    }
+    
+}
---
+++ MutableString.java
@@ -0,0 +1,17 @@
+package String;
+
+public class MutableString {
+
+    public static void main(String[] args) {
+        
+
+        String name = "MD";
+        String firstName = " Rifat";
+
+        name = name.concat(firstName);
+
+        name = name.concat(" Chowdhury");
+        System.out.println(name);
+    }
+    
+}
