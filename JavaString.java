f02147e909869f9698d9d953d3934ab38daf953e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-15 11:57:15 +0600

    Add files via upload

---
+++ JavaString.java
@@ -0,0 +1,11 @@
+package String;
+
+public class JavaString {
+
+    public static void main(String[] args) {
+        
+        String str = new String("I am Rifat");
+        System.out.println(str);
+    }
+    
+}
