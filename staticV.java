70ea6c67cf399ba11495b43a83e4fa00876af40f
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-15 18:12:16 +0600

    Add files via upload

---
+++ staticV.java
@@ -0,0 +1,13 @@
+package Variables;
+
+public class staticV {
+
+    public static String name =" Rifat" ;
+
+    public static void main(String[] args) {
+        
+
+        System.out.println(" Name is "+ staticV.name);
+    }
+    
+}
