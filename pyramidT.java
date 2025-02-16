3c68a013e254f12c3e5d44fec0caccb22d7975c3
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-16 17:58:57 +0600

    Add files via upload

---
+++ pyramidT.java
@@ -0,0 +1,23 @@
+package Loop.Task;
+
+public class pyramidT {
+
+    public static void main(String[] args) {
+        
+
+        for(int i=0; i<=4; i++)
+
+        {
+            for(int j =0;j<=i; j++)
+
+            {
+                System.out.print("1");
+
+            
+            }
+            System.out.println();
+            
+        }
+    }
+    
+}
