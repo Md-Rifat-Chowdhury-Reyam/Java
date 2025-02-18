b4ff6820b3b13ce5e90b0cb00ff7212c00d1a56e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-18 23:42:58 +0600

    Add files via upload

---
+++ NumberTri.java
@@ -0,0 +1,33 @@
+package Loop.Task;
+
+
+public class NumberTri {
+
+
+    public static void printN(int n)
+    {
+        int i,j;
+
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+
+            }
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+
+
+        int n= 5;
+        printN(n);
+        
+    }
+}
