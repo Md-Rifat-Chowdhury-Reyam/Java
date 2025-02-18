e4759249a5064bbc54143477ac033a9529ffff8c
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-18 23:45:53 +0600

    Add files via upload

--- NumberTri.java
+++ NumberTri.java
@@ -9,14 +9,14 @@ public class NumberTri {
         int i,j;
 
 
-        for(i=1;i<=n;i++)
+        for(i=0;i<=n;i++)
         {
             for(j=1;j<=n-i;j++)
             {
                 System.out.print(" ");
 
             }
-            for(j=1;j<=i;j++)
+            for(j=0;j<=i;j++)
             {
                 System.out.print(i+" ");
             }
