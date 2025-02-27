afaedd57aefd6ad5c2c67e61c385fa1c64f9f423
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-27 23:36:20 +0600

    Add files via upload

---
+++ setGet.java
@@ -0,0 +1,27 @@
+package OOP.Inheritance;
+
+public class setGet {
+
+    static String CustomerName;
+    static int CustomerSalary;
+
+
+    static void set(String n, int s)
+    {
+        CustomerName= n;
+        CustomerSalary= s;
+                 
+    }
+
+    static void get()
+    {
+        System.out.println(" Customer Name " +CustomerName);
+        System.out.println("Customer Salary "+ CustomerSalary);
+    }
+
+
+    public static void main(String[] args) {
+        setGet.set("Md. Rifat Chowdhury", 20000);
+        setGet.get();
+    }
+}
