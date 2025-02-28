2a2b9e415a197e6dfdc23dc82f0becc9bb3282b3
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-28 12:10:38 +0600

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,39 @@
+class Hi {
+    
+
+   
+    String n = "HiHello";
+
+
+    public static void main(String[] args) {
+      
+        
+        try {
+           
+            Class<?> c = Class.forName("Hi");
+          
+            
+            Hi o = (Hi) c.getDeclaredConstructor().newInstance();
+          
+            
+            System.out.println(o.n);}
+        
+        catch(ClassNotFoundException e)
+        
+        {
+            e.printStackTrace();
+        }
+        catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
