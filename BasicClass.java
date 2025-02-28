d1983d64f4135c775315dfa60ce661559bb042b7
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-02-28 10:50:02 +0600

    Add files via upload

---
+++ BasicClass.java
@@ -0,0 +1,17 @@
+
+import utill.*;
+public class BasicClass{
+
+    String name;
+    int  id;
+
+    public static void main(String[] args) {
+        
+
+        BasicClass obj = new BasicClass();
+
+        System.out.println(obj.name +"  " + obj.id);
+    }
+
+
+}
