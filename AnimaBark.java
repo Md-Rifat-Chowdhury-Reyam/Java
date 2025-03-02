f4d509f6ecbc4b5cd21a10182cf5c271a9a1b528
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-02 22:17:42 +0600

    Add files via upload

---
+++ AnimaBark.java
@@ -0,0 +1,30 @@
+package Interface;
+
+public interface InnerAnimalBark {
+
+    
+void bark();
+}
+
+
+class Dog implements InnerAnimalBark{
+    @Override
+    public void bark()
+    {
+        System.out.println("Dog is Barking");
+    }
+    
+}
+
+
+
+
+public class AnimaBark {
+    public static void main(String[] args) {
+        
+        Dog d = new Dog();
+        d
+
+    }
+    
+}
