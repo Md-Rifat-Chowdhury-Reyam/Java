5986ec5ebb7355db9afc1ee7b09338cb0efccce6
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-02 12:05:15 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,31 +1,108 @@
-interface Shape {
+abstract class BankAccount{
+    private String AccountNumber;
+    private double balance;
 
-    double area();
-
-    
-} 
-
-class Circle implements Shape
-{
-    private double radius;
-
-    public  Circle (double radius)
+    public BankAccount(String AccountNumber, double balance)
     {
-        this.radius = radius;
+        this.AccountNumber = AccountNumber;
+        this.balance = balance;
+
     }
 
-    public double area()
+    public String getAccountNumber()
+    {return AccountNumber;}
+
+    public double getBalance()
     {
-        return (Math.PI*radius*radius );  
+        return balance;
+    }
+
+    protected void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+    public abstract void deposit(double amount);
+    public abstract void withdraw( double amount);
+}
+
+
+class SavingsAccount extends BankAccount {
+   
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
+  
+    @Override
+    public void deposit(double amount) {
+        
+        setBalance(getBalance() + amount);
+        
+        System.out.println("Deposit of $" + amount + " successful. Current balance: $" + getBalance());
+    }
+
+    
+    @Override
+    public void withdraw(double amount) {
+        
+        if (getBalance() >= amount) {
+           
+            setBalance(getBalance() - amount);
+         
+            System.out.println("Withdrawal of $" + amount + " successful. Current balance: $" + getBalance());
+        } else {
+       
+            System.out.println("Insufficient funds. Withdrawal failed.");
+        }
     }
 }
 
 
-public class Main{
-    public static void main(String[] args) 
-    {
-        Circle c = new Circle(5.0);
-        System.out.println("Area of Circle: "+ c.area());
+class CurrentAccount extends BankAccount {
+    
+    
+    public CurrentAccount(String accountNumber, double balance) {
+        super(accountNumber, balance); 
+    }
+
+ 
+    @Override
+    public void deposit(double amount) {
        
-    } 
+        setBalance(getBalance() + amount);
+        
+        System.out.println("Deposit of $" + amount + " successful. Current balance: $" + getBalance());
+    }
+
+    
+    @Override
+    public void withdraw(double amount) {
+        
+        if (getBalance() >= amount) {
+           
+            setBalance(getBalance() - amount);
+          
+            System.out.println("Withdrawal of $" + amount + " successful. Current balance: $" + getBalance());
+        } else {
+           
+            System.out.println("Insufficient funds. Withdrawal failed.");
+        }
+    }
+}
+ 
+
+public class Main {
+    
+    public static void main(String[] args) {
+       
+        SavingsAccount s = new SavingsAccount("2221", 200);
+        s.deposit(100);
+        s.withdraw(50);
+        s.withdraw(300);
+
+        CurrentAccount c = new CurrentAccount("2222", 500);
+        c.deposit(200); 
+        c.withdraw(100);
+        c.withdraw(700);
+    }
 }
