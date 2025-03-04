f7cf79b84c71c9880f89b8f1936212e4f6346831
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-04 19:22:25 +0600

    Add files via upload

---
+++ programmer.java
@@ -0,0 +1,15 @@
+public class programmer {
+
+    private String name;
+
+    public String getName()
+    {
+        return name;
+    }
+   
+    public void setName(String n)
+    {
+        this.name = n;
+    }
+    
+}
