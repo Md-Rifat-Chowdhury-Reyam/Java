09b0f0a826c024a324f36fa4bdb257d4a98012f7
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-06 00:00:21 +0600

    Add files via upload

---
+++ Bycycle.java
@@ -0,0 +1,16 @@
+public class Bycycle
+{
+
+    public int gear, speed;
+
+    public Bycycle(int g, int s)
+    {
+        this. gear= g;
+        this. speed = s;
+    }
+
+    public String toString()
+    {
+        return (" this bycycle "+gear+" speed now "+ speed+ "kph");
+    }
+}
