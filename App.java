b6806e3adbea13c05e534c053a0735555306489e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-09 01:22:03 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,10 +1,11 @@
-public class App{
-    public static void main(String []args)
-    {
-        Son s = new Son();
-        s.print();
-        
-        
+public class App {
+
+    public static void main(String[] args) {
+        Rifat r = new Rifat();
+        Daughter d = new Daughter();
+        r.print(" Rifat","Mila");
+        r.print();
+        d.print();
+
     }
-   
 }
