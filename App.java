d68a722af01bbf18ad0bdb9a56b2ab7d049657c5
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-09 13:55:47 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -4,5 +4,15 @@ public class App {
         Child c = new Child();
         c.printF();
         c.printM();
+
+        SolarSystem s = new SolarSystem();
+        Earth e = new Earth();
+        Mars m = new Mars();
+        Moon mo = new Moon();
+
+        System.out.println(m instanceof SolarSystem);
+        System.out.println(e instanceof SolarSystem);
+        System.out.println(mo instanceof SolarSystem);
+        System.out.println(mo instanceof Earth);
     }
 }
---
+++ SolarSystem.java
@@ -0,0 +1,18 @@
+ class SolarSystem {
+
+
+    
+}
+
+class Earth extends SolarSystem{
+
+}
+
+class Mars extends SolarSystem{
+
+}
+
+class Moon  extends Earth{
+
+}
+
