b77b7acc8e8349de66cc6a097673c5291e980bc1
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-10 14:58:32 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,9 +1,9 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-      
-        Person p = new Father();
-         Person p1 = new Husband();
-        p.Print();
-        p1.Print();
+public class App
+{
+    public static void main(String[] args) {
+        
+        
+        System.out.println(" the multiply is "+Multiply.Mul(2, 3));
+        System.out.println(" the multiply is "+ Multiply.Mul(5.5, 6.3));
     }
 }
---
+++ Multiply.java
@@ -0,0 +1,13 @@
+public class Multiply
+{
+    static int Mul(int a, int b)
+    {
+        return  (a*b);
+
+    }
+
+    static double Mul(Double a, Double b)
+    {
+        return a*b;
+    }
+}
