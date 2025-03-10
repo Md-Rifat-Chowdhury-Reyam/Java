7e1a5534f97c4f6993075643346082b1855c8d75
MD. Rifat Chowdhury <md.rifat.chowdhury.reyam@gmail.com>
2025-03-10 14:59:28 +0600

    Overloading Polymorphism

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App
+{
+    public static void main(String[] args) {
+        
+        
+        System.out.println(" the multiply is "+Multiply.Mul(2, 3));
+        System.out.println(" the multiply is "+ Multiply.Mul(5.5, 6.3));
+    }
+}
