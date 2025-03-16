d56c59a8a4cc740828ea6ef5e31dde946c6267d9
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-16 14:44:39 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,10 +1,14 @@
-public class App
-{
-    public static void main(String[] args) {
-        
-        int [][] arr ={{1,2,3},{4,5,6},{7,8,9}};
+public class App{
+     public static void main(String[] args) {
+        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
 
-        System.out.println("The first element of first row "+arr[0][0]);
-        System.out.println("The 3rd element of 2nd row "+arr[1][2]);
-    }
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j<3; j++)
+            {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
+        }
+     }
 }
