ce5ac19f3feae057ffc3f65a2a44f9eab678f5e9
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-16 16:26:31 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,14 +1,35 @@
-public class App{
-     public static void main(String[] args) {
-        int [][] arr = {{1,2,3},{4,5,6},{7,8,9}};
+public class App
+{
+    static boolean  searchMetrix(int [][] arr, int x)
+    {
+        int m = arr.length;
+        int n = arr[0].length;
 
-        for(int i = 0; i < 3; i++)
+        for(int i = 0; i<m; i++)
         {
-            for(int j = 0; j<3; j++)
+            for(int j = 0; j<n; j++)
             {
-                System.out.print(arr[i][j]+" ");
+                if(arr[i][j] == x)
+                {
+                    return true;
+                }
             }
+            
+        }return false;
+
+        
+    }
+
+    public static void main(String[] args) {
+        int x = 3;
+        int [][]arr ={{1,2,3},{4,3,5},{2,34,5}};
+        if (searchMetrix(arr, x))
+         {
+
+            System.out.println("True "+ x + " in the array"+ arr.length);
+        }
+        else{
             System.out.println("");
         }
-     }
+    }
 }
