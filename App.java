495c3c51e796b201d1855986da883d67befb316a
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-16 12:53:17 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -2,8 +2,9 @@ public class App
 {
     public static void main(String[] args) {
         
-        
-        System.out.println(" the multiply is "+Multiply.Mul(2, 3));
-        System.out.println(" the multiply is "+ Multiply.Mul(5.5, 6.3));
+        int [][] arr ={{1,2,3},{4,5,6},{7,8,9}};
+
+        System.out.println("The first element of first row "+arr[0][0]);
+        System.out.println("The 3rd element of 2nd row "+arr[1][2]);
     }
 }
