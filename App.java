8d38f8ca29b3cd0e7e4e8362f5d945a7c1960a1c
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-18 16:36:58 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,41 +1,54 @@
 import java.util.Arrays;
 
-public class App {
-
-    public static  void sortAr(int [][] arr)
+public class App{
+    static int[][] transpose(int[][] mat, int row, int col)
     {
-        for(int []row : arr)
+        int[][] tr = new int[col][row];
+        for(int i = 0; i < row; i++)
         {
-           Arrays.sort(row);
+            for(int j= 0; j <col; j++)
+            {
+                tr[j][i] = mat[i][j];
+
+            }
+        }
+        return tr;
+    }
+
+    static void sortRow(int[][] arr)
+    {
+        for(int i = 0; i < arr.length; i++)
+        {
+            Arrays.sort(arr[i]);
+
+        }
+    }
+
+    static void sortCol(int[][] mat, int r, int c)
+    {
+        int[][] arr = transpose(mat, r, c);
+        sortRow(arr);
+
+        mat = transpose(arr, r, c);
+
+        for(int i =0 ; i < r; i++)
+        {
+            for(int j = 0; j < c; j++)
+            {
+                System.err.print(mat[i][j]+ " ");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int [][] arr1 ={
-            {77, 11, 22, 3},
-            {11, 89, 1, 12},
-            {32, 11, 56, 7},
-            {11, 22, 44, 33}
+        int[][] mat ={{111,2,5554},
+    {443,4,55,632},{23423,3534,56}};
 
-        };
+        int r = mat.length;
+        int c = mat[0].length;
+        sortCol(mat, r, c);
 
-        sortAr(arr1);
-        System.out.println(" [ ");
-        for(int []row : arr1)
-        {
-            System.out.print( " [");
-            for(int i =0; i<arr1.length; i++)
-            {
-                if(i>0)
-                {
-                    System.out.print(" ' ");
-                }
-                System.out.print(row[i]);
-            }
-            System.out.println(" ] ");
-
-        }
     }
-    
-
 }
+
