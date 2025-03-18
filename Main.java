e1257ff2b27762ab636f985cb991d597784871e4
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-18 16:13:55 +0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,58 @@
 import java.util.Arrays;
 
-public class Main{
+public class Main
+   {
 
-    public static void sortA(int [][] arr)
+    static int[][] transpose(int[][] mat, int row, int col)
     {
-        for(int [] row : arr)
+        int[][]tr = new int[col][row];
+
+        for(int i = 0; i < row; i++)
         {
-            Arrays.sort(row);
+            for(int j =0; j < col; j++)
+            {
+                tr[j][i] = mat[i][j];
+
+            }
+        }
+        return tr;
+    }
+
+    static void sortRow(int[][] arr)
+    {
+        for(int i = 0; i < (int)arr.length; i++ )
+        {
+            Arrays.sort(arr[i]);
+        }
+    }
+    static void sortCol(int[][]mat, int r, int c)
+    {
+        int[][] arr = transpose(mat, r, c);
+        sortRow(arr);
+
+        mat = transpose(arr, c, r);
+
+        for(int i = 0; i < r; i++)
+        {
+            for( int j = 0; j < c; j++)
+            {
+                System.out.print(mat[i][j]+ " ");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
-        int [][] arr =  {
-            {77, 11, 22, 3},
-            {11, 89, 1, 12},
-            {32, 11, 56, 7},
-            {11, 22, 44, 33}
-        };
+        int [][] mat = { { 1, 6, 10, 5, 9},
+        { 8, 5, 9,5, 9 },
+        { 9, 4, 15,5, 9 },
+        { 9, 4, 15,5, 9 },
+        { 7, 3, 60,5, 9 } };
 
-        sortA(arr);
+        int r = mat.length;
+        int c = mat[0].length;
 
+        sortCol(mat, r, c);
 
-        System.out.println(" .. ");
-        for (int [] row : arr)
-        {
-            System.out.println(" ' ");
-            for(int i =0; i< arr.length; i++)
-            {
-                if(i>0)
-                {
-                    System.out.print(",");
-                }
-                System.out.print(row[i]);
-            }
-        }
     }
-    
-}
+   }
