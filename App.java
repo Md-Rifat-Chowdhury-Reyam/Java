33494e98abd2a50de9c2990d67d94a185dc3951e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-19 00:23:21 +0600

    Add files via upload

--- App.java
+++ App.java
@@ -3,51 +3,51 @@ import java.time.Instant;
 
 public class App
 {
-    static void rowWise(int[][] arr)
-    {
-        int row = arr.length;
-        int col = arr[0].length;
+     static void rowWise(int[][] arr)
+     {
+        int r = arr.length;
+        int c= arr[0].length;
 
-        for(int i = 0; i <row; i++)
-        { for(int j = 0; j< col; j++)
-            {
-                arr[i][j]++;
-            }
-         
-        
-        }
-    }
-
-
-    static void colWise(int[][] arr)
-    {
-        int row =arr.length;
-        int col = arr[0].length;
-
-        for(int i = 0; i< col; i++)
+        for(int i = 0 ; i < r; i++)
         {
-            for( int j = 0; j< row  ; j++)
+            for(int j = 0; j < c; j++)
+            {
+                arr[i][j]++;
+            }
+
+        }
+     }
+
+     static void colWise(int [][]arr)
+     {
+        int r= arr.length;
+        int c = arr[0].length;
+
+        for(int j = 0; j< c; j++)
+        {
+            for(int i = 0; i < r; i++ )
             {
                 arr[i][j]++;
 
             }
+
         }
-    }
 
+     }
 
-    public static void main(String[] args) {
-        
+     public static void main(String[] args) {
         int n = 1000;
-        int [][] arr = new int[n][n];
+        int [][] arr= new int[n][n];
 
         Instant start = Instant.now();
         rowWise(arr);
-        Instant end = Instant.now();
-        System.out.println("Row access time "+ Duration.between(start, end));
+        Instant end = Instant. now();
+        System.out.println("Row check time "+ Duration.between(start, end));
 
-         start = Instant.now();
+
+        start = Instant.now();
         colWise(arr);
-         end = Instant.now();
-        System.out.println("Column access time"+ Duration.between(start, end));
-    }
+        end = Instant.now();
+        System.out.println("Col check in "+ Duration.between(start, end));
+     }
 }
