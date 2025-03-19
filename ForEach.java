2c29319f2250530723b76c8b0e3cb3c6445cdd5f
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-19 12:54:08 +0600

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach{
+    public static void main(String[] args) {
+        
+        int[] arr ={1,2,3,4,5,6};
+        
+
+        for(int row : arr)
+        {
+            System.out.println(row+ " ");
+        }
+    }
+    
+}
