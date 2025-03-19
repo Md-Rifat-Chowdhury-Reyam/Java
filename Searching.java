78bc9ac1c01e59bc4fc25b97dce8fa65267cb9ab
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-19 13:07:06 +0600

    Add files via upload

--- Searching.java
+++ Searching.java
@@ -4,17 +4,24 @@ public class Searching {
         
         int [] arr = {2,3,4,56,7};
         int search = 3;
+        boolean found = false;
 
         for(int i = 0; i< arr.length; i++)
         {
             if(arr[i] == search)
-            {
-                System.out.print("True"+" ");
-            }
-            else{
-                System.out.print("False"+" ");
-            }
+           {
+            found = true;
+           }
 
+        }
+
+        if (found) {
+            System.out.println(" Founded");
+            
+        }
+        else
+        {
+            System.out.println(" Not ");
         }
     }
     
