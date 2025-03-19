51c34aa637e613a89dea52050b1ec2ba571d2b6e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-19 12:35:30 +0600

    Add files via upload

---
+++ LinearTA.java
@@ -0,0 +1,16 @@
+public class LinearTA {
+    public static void main(String[] args) {
+        
+        int [] arr = {1,2,3,45,6,7};
+        int row = arr.length;
+        System.out.print("The linear traversal ");
+
+        for(int i = 0; i< row; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+        
+
+    }
+    
+}
