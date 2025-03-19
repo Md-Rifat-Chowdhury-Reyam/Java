124382c894ed58a3fe5037fc10d57192c154ab5a
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-03-19 13:14:25 +0600

    Add files via upload

---
+++ Modifyig.java
@@ -0,0 +1,15 @@
+public class Modifyig
+{
+    public static void main(String[] args) {
+        
+        int [] arr = { 1,3,3,54,65,22,3};
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] += 5;
+
+            System.out.print(arr[i]+ " ");
+        }
+
+    }
+}
