45531c22493101cb047af2d525ade336e8c3bb5e
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-04 15:15:54 +0600

    Add files via upload

--- FormWithSwing.java
+++ FormWithSwing.java
@@ -1,5 +1,8 @@
-import java.awt.FlowLayout;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
@@ -13,20 +16,45 @@ public class FormWithSwing {
     {
 
         JFrame jframe = new JFrame("Sign Up Form");
-        jframe.setSize(600, 600);
+        jframe.setSize(600, 300);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jframe.setVisible(true);
         jframe.setLayout(new FlowLayout());
+        jframe.setVisible(true);
 
 
         JLabel jlabelHeading = new JLabel("Sign Up Form", JLabel.CENTER);
-
         jframe.add(jlabelHeading);
+        
 
-        JLabel jlabelName = new JLabel("Name :", JLabel.CENTER);
+        JLabel jlabelName = new JLabel("Name :");
         jframe.add(jlabelName);
+        JButton JbtName = new JButton(" Name");
+        jframe.add(JbtName);
 
+        JLabel jlabAddress = new JLabel("Address :");
+        jframe.add(jlabAddress);
+        JButton JbtAddreess = new JButton("Address");
+        jframe.add(JbtAddreess);
 
+        JbtName.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+                System.out.println("Name button clicked " + JbtName.getText());
+                JbtName.setText("Md Rifat Chowdhury");
+            }
+        });
+
+        JbtAddreess.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+            JbtAddreess.setText("Dhaka, Bangladesh"); 
+            System.out.println("Address button clicked " + JbtAddreess.getText());
+            }
+        });
+
+       
         
     }
     public static void main(String[] args) {
