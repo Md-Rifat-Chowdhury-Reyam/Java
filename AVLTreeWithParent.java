eb8d29b9516dd679ba5dc4d85829497f57e66c33
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-05 07:17:01 +0600

    Add files via upload

--- AVLTreeWithParent.java
+++ AVLTreeWithParent.java
@@ -1,6 +1,6 @@
 public class AVLTreeWithParent {
 
-    public static void updateHeight(AVLTreeWithParent root)
+    public static void updateHeight(AVLwithparent root)
     {
         if( root != null)
         {
@@ -8,6 +8,35 @@ public class AVLTreeWithParent {
             int rightHeight = (root.right != null) ? root.right.height : 0;
             root.height = Math.max (leftHeight, rightHeight) + 1; 
         }
+    }
+
+    public static  AVLwithparent LLRotation(AVLwithparent root)
+    {
+        AVLwithparent tempNode = root.left;
+        root.left = tempNode.right;
+
+        if(tempNode.right != null)
+        { 
+            tempNode.right.par = root;
+        }
+
+        tempNode.right = root;
+        tempNode.par = root.par;
+        root.par = tempNode;
+
+        if(tempNode.par != null)
+        {
+            if(root.key < tempNode.par.key)
+            {
+                tempNode.par.left = tempNode;
+            }
+            else{
+                tempNode.par.right = tempNode;
+            }
+
+        updateHeight(root);
+        updateHeight(tempNode);
+        return tempNode;
     }
     
 }
