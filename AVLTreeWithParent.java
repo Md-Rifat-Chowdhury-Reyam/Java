0badc5b4ea0a6bd72d338c5e506b8fb1aa6d5856
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-04-05 18:16:02 +0600

    Add files via upload

--- AVLTreeWithParent.java
+++ AVLTreeWithParent.java
@@ -85,4 +85,78 @@ public class AVLTreeWithParent
         return RRRotation(root);
     }
   
+
+    public static AVLwithparent insert(AVLwithparent root, AVLwithparent parent, int key)
+    {
+
+        if(root == null)
+        {
+            root = new AVLwithparent(key);
+            root.par = parent;
+        }
+
+        else if ( root.key > key)
+        {
+            root.left = insert (root.left, root, key );
+
+            int leftHeight = (root.left != null) ? root.left.height : 0;
+            int rightHeight = (root.right != null) ? root.right.height : 0;
+            if(Math.abs(leftHeight - rightHeight) == 2)
+            {
+                if(key < root.left.key)
+                {
+                    root = LLRotation(root);
+                }
+                else{
+                    root = LRRotation(root);
+                }
+            }
+        }
+        else if ( root.key < key)
+        {
+            root.right = insert(root.right,root,key);
+            int leftHeight = (root.left != null) ?  root.left.height : 0;
+            int rightHeight = ( root.right != null) ? root.right.height : 0;
+            if(Math.abs(leftHeight - rightHeight) == 2)
+            {
+                if(key < root.right.key)
+                {
+                    root = RLRotation(root);
+                }
+                else{
+                    root = RRRotation(root);
+                }
+                
+            }
+        }
+
+        updateHeight(root);
+        return root;
+
+    }
+
+    public static void  printPreOrder(AVLwithparent root)
+    {
+        if( root != null)
+        {
+            String parentKey = ( root.par != null) ? Integer.toString(root.par.key) : "null";
+            System.out.println("Node: " + root.key + ", Parent Node: " + parentKey + ", Height: " + root.height);
+            printPreOrder(root.left);
+            printPreOrder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        AVLwithparent root = null;
+        int [] keys = {10, 20, 30, 40, 50, 25};
+        for (int key : keys)
+        {
+            root = insert(root, root, key);
+        }
+        System.out.println("Preorder traversal of the constructed AVL tree is: ");
+        printPreOrder(root);    
+    }
+
+        
 }
+
